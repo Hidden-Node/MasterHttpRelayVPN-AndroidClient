bc9c7bab3e5e10f68c1959f95963b1888a7df853
Hidden-Node <mr.regista6@gmail.com>
2026-04-23 19:54:14 +0330

    Fix versioning and resolve build issues

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,16 +7,6 @@ plugins {
     id("com.chaquo.python")
 }
 
-python {
-    buildPython("/usr/bin/python3")
-    pip {
-        install("cryptography>=41.0.0")
-        install("h2>=4.1.0")
-        install("brotli>=1.1.0")
-        install("zstandard>=0.22.0")
-    }
-}
-
 android {
     namespace = "com.masterhttprelay.vpn"
     compileSdk = 35
